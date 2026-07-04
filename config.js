7f78ceb7bddeea603e95160e4d6e24c461a1cefc
PFTSAFETY <hllpftsafety@gmail.com>
2026-07-04 17:00:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
  * Update API_URL after each new Apps Script deployment.
  */
 window.PFT_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/REPLACE_WITH_YOUR_DEPLOYMENT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxbsW36PywwGO0Odybb7hCwPbGI4CoZS2wX3rzAdvQesoeD0ASmWiVjjJLfYda376es/exec',
   PORTAL_URL: 'https://pftsafety.github.io/Portal/',
   SESSION_KEY: 'pft_session'
 };
